10edd374bc4bfd7f05994352ef88dd10dfd3d9ad
GruvGTR-blip <GruvGTR@icloud.com>
2026-06-14 15:22:02 +0300

    Initialize project: add brain_games module and configure package

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env python3
+"""Brain Games entry point."""
+
+def main():
+    """Main function of the game."""
+    print("Welcome to the Brain Games!")
+
+if __name__ == "__main__":
+    main()
