2a6a3bd69004e273889d07e4d932f427c1fc627f
GruvGTR-blip <GruvGTR@icloud.com>
2026-06-16 18:21:52 +0300

    fix

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,13 +2,13 @@
 """Фаил запуска. Он здоровается с игроком, после чего запускает игру."""
 
 from brain_games.cli import welcome_user
-
 from brain_games.games.even import run_even
 
+
 def main():
-	"""Приветствие, запуск """
-	name = welcome_user()
-	run_even(name)
+    """Приветствие, запуск """
+    name = welcome_user()
+    run_even(name)
 
 if __name__ == "__main__":
-	main()
+    main()
